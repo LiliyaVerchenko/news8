c48c91b584d2fe3d842102172e9b7ab3ee1d07b8
Liliya Verchenko <ip_verchenko@inbox.ru>
2020-06-29 17:30:56 +0300

    format.json.py changed

--- format.json.py
+++ format.json.py
@@ -1,29 +1,27 @@
-
+import json
+from pprint import pprint
 def read_news(file):
-    import json
-    from pprint import pprint
     with open ('newsafr.json', encoding='utf-8') as f:
         json.data = json.load(f)
         news_list = json.data['rss']['channel']['items']
+        list5 = []
         for new in news_list:
-            list_1 = new['description'].strip().lower().split()
-    return list_1
-
-# read_news('newsafr.json')
+            list5.extend(new['description'].split(' '))
+    return list5
 
 def choose_words():
     words_dict = {}
     ld = read_news('newsafr.json')
-    count = 0
     for word in ld:
         if len(word) > 6:
             if word in words_dict:
                 words_dict[word] += 1
             else:
                 words_dict[word] = 1
+    return words_dict
 
-    return print(words_dict)
-choose_words()
-
-#return print(sorted(words_dict.items(), key=lambda х: х[1], reverse=True)[:10])
-
+def top_word():
+    words_dict1 = sorted(choose_words().items(), key=lambda х: х[1], reverse=True)[:10]
+    for i, word in enumerate(words_dict1, 1):
+        print(f'{i} место: "{word[0]}" - встречается в тексте {word[1]} раз')
+top_word()
